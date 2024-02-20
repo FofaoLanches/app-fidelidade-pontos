e894b1217a32090e42a0d7718b9d7c4a8079526f
fofaolanches <fofaotaquara@gmail.com>
2024-02-20 17:27:05 -0300

    chore gap

--- src/components/List/listIconButtonRoot.tsx
+++ src/components/List/listIconButtonRoot.tsx
@@ -2,5 +2,5 @@ import { SomeChildInterface } from "@/types";
 
 export const ListIconButtonRoot: React.FC<SomeChildInterface> = (props) => {
   const { children } = props;
-  return <div className="flex flex-col w-[80px] justify-between">{children}</div>;
+  return <div className="flex flex-col w-[80px] gap-4 justify-between">{children}</div>;
 };
