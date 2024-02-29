71e7f8b8d1d022e410a48f09a3f1a931fa5c74c7
flama <flamarionpinto@sou.faccat.br>
2024-02-29 15:12:44 -0300

    fix: cloudflare url

--- next.config.js
+++ next.config.js
@@ -4,13 +4,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "fofao-lanches-test-bucket.d561668156bfc2b4598ef69ad0cc164a.r2.cloudflarestorage.com",
-        port: "",
-        pathname: "/products/**",
-      },
-      {
-        protocol: "https",
-        hostname: "fofao-lanches-test-bucket.cdf5b6507fa4af4344fbab896825f3c6.r2.cloudflarestorage.com",
+        hostname: "fofao-lanches-bucket.cdf5b6507fa4af4344fbab896825f3c6.r2.cloudflarestorage.com",
+        /* hostname: "fofao-lanches-test-bucket.d561668156bfc2b4598ef69ad0cc164a.r2.cloudflarestorage.com" TEST URL */
         port: "",
         pathname: "/products/**",
       },
