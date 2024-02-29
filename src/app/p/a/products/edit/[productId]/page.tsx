3b95aad66d94062c794128502a2b8dd7d8c6b5ee
fofaolanches <fofaotaquara@gmail.com>
2024-02-29 17:41:45 -0300

    client edit produto

--- src/app/p/a/products/edit/[productId]/page.tsx
+++ src/app/p/a/products/edit/[productId]/page.tsx
@@ -1,23 +1,37 @@
-import React from "react";
+"use client";
+import { isEmpty } from "lodash";
+import { useSession } from "next-auth/react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { getEndpointBaseUrlAPIS } from "@/helpers";
-import { useServerSession } from "@/hooks";
 import { EditProductIdPageInterface, GetProductsInterface } from "@/types";
 
 import { ClientPage } from "./clientPage";
 
-export default async function Page(props: EditProductIdPageInterface) {
+export default function Page(props: EditProductIdPageInterface) {
   const { searchParams } = props;
-  const session = await useServerSession();
+  const [product, setProduct] = useState({} as GetProductsInterface);
+  const session = useSession();
 
-  const req = await fetch(`${getEndpointBaseUrlAPIS()}/products/${searchParams.productId}`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${session.user?.token}`,
-    },
-  });
+  const teste = useCallback(async () => {
+    if (session.data?.user.token) {
+      const req = await fetch(`${getEndpointBaseUrlAPIS()}/products/${searchParams.productId}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${session.data?.user.token}`,
+        },
+      });
 
-  const res: GetProductsInterface = await req.json();
+      const res: GetProductsInterface = await req.json();
+      setProduct(res);
+    }
+  }, [session]);
 
-  return <ClientPage product={res} token={session.user?.token} />;
+  useEffect(() => {
+    teste();
+  }, []);
+
+  if (isEmpty(product)) return <></>;
+
+  return <ClientPage product={product} token={session.data?.user.token} />;
 }
