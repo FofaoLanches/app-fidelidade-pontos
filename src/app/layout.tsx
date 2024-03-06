3a1d06b6d04db8742c197aa3d385baa9e69d478d
flama <flamarionpinto@sou.faccat.br>
2024-03-06 10:59:06 -0300

    chore: maintenance off

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 
-import { BasicAuth, Screen } from "@/components";
+import { /*BasicAuth,*/ Screen } from "@/components";
 import SessionAuthenticateProvider from "@/context/sessionProvider";
-import { getEndpointBaseUrlClient } from "@/helpers";
+// import { getEndpointBaseUrlClient } from "@/helpers";
 import { NextLayoutComponentInterface } from "@/types";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,18 +17,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children, session }: NextLayoutComponentInterface) {
-  const clientBaseUrl = getEndpointBaseUrlClient();
+  // const clientBaseUrl = getEndpointBaseUrlClient();
 
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={inter.className}>
-        {/*TODO: Remove BasicAuth after production*/}
-        <BasicAuth clientBaseUrl={clientBaseUrl}>
-          <Toaster position="top-right" reverseOrder={false} />
-          <SessionAuthenticateProvider session={session}>
-            <Screen>{children}</Screen>
-          </SessionAuthenticateProvider>
-        </BasicAuth>
+        {/* <BasicAuth clientBaseUrl={clientBaseUrl}> */}
+        <Toaster position="top-right" reverseOrder={false} />
+        <SessionAuthenticateProvider session={session}>
+          <Screen>{children}</Screen>
+        </SessionAuthenticateProvider>
+        {/* </BasicAuth> */}
       </body>
     </html>
   );
