c76d9cc47c9bd706488183dce104c7d02ed31218
fofaolanches <fofaotaquara@gmail.com>
2024-03-23 15:17:29 -0300

    images h

--- src/app/p/gotched-points/clientPage.tsx
+++ src/app/p/gotched-points/clientPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useFormik } from "formik";
-import { isEmpty } from "lodash";
+import { isEmpty, truncate } from "lodash";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
@@ -59,12 +59,12 @@ export const ClientPage: React.FC<GotchedPointClientPageInterface> = (props) => 
   }
 
   return (
-    <div className="relative w-full px-[5%]">
+    <div className="relative w-full pb-[20%] px-[5%]">
       <span className="text-fontsColor-800 font-medium">Resgate seus pontos:</span>
       <form className="mt-2">
         <ul className="overflow-y-auto lg:overflow-y-scroll h-full space-y-4 max-h-[80vh] lg:max-h-[35vh] lg:pr-2 hide-scrollbar">
           {products.map((product) => {
-            let { amount_points, description, display_name, id, image_url = "" } = product;
+            let { amount_points, description = "...", display_name, id, image_url = "" } = product;
             const isChecked = values.checkedProducts.includes(id);
 
             return (
@@ -90,7 +90,7 @@ export const ClientPage: React.FC<GotchedPointClientPageInterface> = (props) => 
                         src={image_url || "/empty_product.png"}
                         blurDataURL={image_url || "/empty_product.png"}
                         placeholder="blur"
-                        className="border-r-[1px] border-solid border-fundo-300 w-[122px] h-[92px]"
+                        className="border-r-[1px] border-solid border-fundo-300 w-[122px] min-h-[104px] object-cover"
                         width={200}
                         height={200}
                         quality={100}
@@ -99,7 +99,7 @@ export const ClientPage: React.FC<GotchedPointClientPageInterface> = (props) => 
                         {display_name}
 
                         <div className="flex justify-between text-fontsColor-700 font-medium text-sm pt-1">
-                          {!isEmpty(description) && <span>{description}</span>}
+                          <span>{truncate(description, { length: 46 })}</span>
                         </div>
                       </div>
                     </div>
@@ -113,7 +113,7 @@ export const ClientPage: React.FC<GotchedPointClientPageInterface> = (props) => 
       {!isEmpty(values.checkedProducts) && (
         <Button
           variant="button"
-          className="text-fontsColor-200 absolute bottom-[-50px] max-w-[90%]"
+          className="absolute text-fontsColor-200 bottom-0 max-w-[90%]"
           onClick={() => handleSubmit()}
           isLoading={isSubmitting}
         >
