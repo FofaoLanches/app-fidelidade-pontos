5e79478189baed7d543441cc1ab355f838459957
fofaolanches <fofaotaquara@gmail.com>
2024-03-25 14:18:11 -0300

    header zindex

--- src/components/Header/without.tsx
+++ src/components/Header/without.tsx
@@ -6,5 +6,5 @@ import { WithoutInterface } from "@/types";
 export const Without: React.FC<WithoutInterface> = (props) => {
   const { className } = props;
 
-  return <div className={twMerge("fixed top-0 left-0 w-screen h-[40px] shadow-2xl", className)} />;
+  return <div className={twMerge("fixed top-0 left-0 w-screen h-[40px] shadow-2xl z-[1]", className)} />;
 };
