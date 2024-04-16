bb0e5d967d9a66654ef7c87a66866da781ce54ce
Flamarionfp <flamarionpinto@sou.faccat.br>
2024-04-16 17:30:49 -0300

    feat: redirect on click notification

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,29 @@
+function parseBody(event) {
+    const text = event.data?.text() ?? "";
+
+    return JSON.parse(text);
+}
+
 self.addEventListener("push", function (event) {
-    const body = event.data?.text() ?? "";
+    try {
+        const { message, redirectUrl } = parseBody(event);
    
-    event.waitUntil(
-      self.registration.showNotification("Fofão Lanches - Pontos de Fidelidade", {
-        body,
-        icon: "/mascote_sem_fundo.png",  
-      }),
+        event.waitUntil(
+            self.registration.showNotification("Fofão Lanches - Pontos de Fidelidade", {
+                body: message,
+                icon: "/mascote_sem_fundo.png",
+                data: { url: redirectUrl }   
+            }),
     );
+    } catch(error) {
+        console.error(error);
+    }
   });
+
+self.addEventListener('notificationclick', function(event) {
+    const { notification } = event;
+    const { url } = notification.data;
+
+    clients.openWindow(url);
+    event.notification.close();
+}, false);
