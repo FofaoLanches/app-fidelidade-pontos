76b413139d4d43d0e3ddbf4e56c5cdce899d4449
Flamarionfp <flamarionpinto@sou.faccat.br>
2024-04-16 13:54:53 -0300

    fix: service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
-self.addEventListener('push', function(event) {
-    console.log("DEBUG event", event);
-
-    const body = event.data?.text() ?? '';
-
-    event.waitUntil(self.registration.showNotification('Fofão Lanches - Pontos de Fidelidade'), {
-        body
-    });
-});
+self.addEventListener("push", function (event) {
+    const body = event.data?.text() ?? "";
+   
+    event.waitUntil(
+      self.registration.showNotification("Fofão Lanches - Pontos de Fidelidade", {
+        body,
+      }),
+    );
+  });
