114a948f18e366d2096ca8662854040bd06c757b
Flamarionfp <flamarionpinto@sou.faccat.br>
2024-04-17 09:03:49 -0300

    chore: remove log

--- src/hooks/usePushNotifications/index.ts
+++ src/hooks/usePushNotifications/index.ts
@@ -8,31 +8,30 @@ export function usePushNotifications() {
 
   const handlePushNotifications = useCallback(async () => {
       if (typeof window === 'undefined') return;
-          navigator.serviceWorker.register('/service-worker.js').then(async serviceWorker => {
+
+      navigator.serviceWorker.register('/service-worker.js').then(async serviceWorker => {
      
-          let subscription = await serviceWorker.pushManager.getSubscription();
+        let subscription = await serviceWorker.pushManager.getSubscription();
     
-          if (!subscription) {
-            const response = await fetch("/api/notifications");
-            const { publicKey } = await response.json();
-            subscription = await serviceWorker.pushManager.subscribe({
-              userVisibleOnly: true,
-              applicationServerKey: publicKey
-            });
+        if (!subscription) {
+          const response = await fetch("/api/notifications");
+          const { publicKey } = await response.json();
+          subscription = await serviceWorker.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: publicKey
+          });
 
 
-            await fetch("/api/notifications", {
-              method: "POST",
-              body: JSON.stringify({
-                user_id: session.data?.user.id,
-                subscription,
-              })
-           });
-          };
-
-          console.log("DEBUG subscription", subscription)
+          await fetch("/api/notifications", {
+            method: "POST",
+            body: JSON.stringify({
+              user_id: session.data?.user.id,
+              subscription,
+            })
         });
-      }, [session.data?.user.id]);
+      };
+    });
+  }, [session.data?.user.id]);
 
     return {
         handlePushNotifications
